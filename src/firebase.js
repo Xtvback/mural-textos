ff8faebb81204874ef5239d2824399cf0de8fc3b
Xtvback <xtvback@outlook.com>
2026-01-20 22:39:53 +0000

    Atualizar o firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,15 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { initializeApp } from "firebase/app"
+import { getFirestore } from "firebase/firestore"
 
 const firebaseConfig = {
-  apiKey: "A tua chave aqui",
-  authDomain: "o-teu-projeto.firebaseapp.com",
-  projectId: "o-teu-projeto",
-  storageBucket: "o-teu-projeto.appspot.com",
-  messagingSenderId: "123456789",
-  appId: "1:123456789:web:abcde"
-};
+  apiKey: "AIzaSyDx8FnnjQL2Gio0OByhkmfwk5r_hGj39bc",
+  authDomain: "onde-o-silencio-nao-chega.firebaseapp.com",
+  projectId: "onde-o-silencio-nao-chega",
+  storageBucket: "onde-o-silencio-nao-chega.firebasestorage.app",
+  messagingSenderId: "100267491281",
+  appId: "1:100267491281:web:545eb0fbf2a2ba74262af0",
+  measurementId: "G-WVP02GEMRM"
+}
 
-const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
+const app = initializeApp(firebaseConfig)
+export const db = getFirestore(app)
